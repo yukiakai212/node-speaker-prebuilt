c0808014c55606192d16b0b0286ae4af60354e68
Yuki <yuki.akai.rz.212@gmail.com>
2024-12-20 10:16:17 +0700

    fix

--- scripts/extract-binary.js
+++ scripts/extract-binary.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const tar = require('tar');
 
-const outputDir = path.join(import.meta.dirname, './build/Release');
+const outputDir = path.join(__dirname, './build/Release');
 
 if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir, { recursive: true });
 }
 
-let binFolder = path.join(import.meta.dirname, 'build/stage/');
+let binFolder = path.join(__dirname, 'build/stage/');
 let name = readdirSync(binFolder)[0];
 let file = fs.createReadStream(path.join(binFolder, name));
 file.pipe(tar.x({
